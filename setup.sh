3573634bc115edef172246cfd587955cae3fbe74
Troy Moo Penn <tmoopenn@Troys-MacBook-Pro-2.local>
2020-04-05 21:38:02 -0400

    added a setup script and more changes. Downsampling needs to be changed to play nice with convolution sizes in U-net

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# PyTorch and scikit learn
+pip install scikit-learn
+
+# Baselines for Atari preprocessing
+# Tensorflow is a dependency, but you don't need to install the GPU version
+pip install tensorflow
+pip install git+git://github.com/openai/baselines
+
+# pytorch-a2c-ppo-acktr for RL utils
+pip install git+git://github.com/ankeshanand/pytorch-a2c-ppo-acktr-gail
+
+# Clone and install package
+pip install -r atari-representation-learning/requirements.txt
+pip install git+git://github.com/mila-iqia/atari-representation-learning.git
+
+# install requirements file 
+pip install -r requirements.txt
