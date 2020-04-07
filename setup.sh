a1fedd1ee05b79b878a8541460a8c170f88e87ff
Troy Moo Penn <tj-mp@hotmail.com>
2020-04-07 21:21:22 +0000

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
+# use pyenv to configure global python environment 
+curl https://pyenv.run | bash
+exec
+pyenv install 3.7-dev
+pyenv global 3.7-dev
+
 # configure system and python dependencies 
 sudo apt update
-sudo apt-get -y install build-essential checkinstall software-properties-common
+sudo apt-get -y install python3-pip python-pip  build-essential checkinstall software-properties-common
 sudo apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
-# install python
-sudo apt-get -y install python3-dev
-py_version=$(python3 --version)
-echo "Installed python version $py_version"
+# install developer python package
+#sudo apt-get -y install python3-dev
+#py_version=$(python3 --version)
+#echo "Installed python version $py_version"
 
 # alias python and add to bashrc file
-echo 'alias python=python3.7' >> ~/.bashrc
+#echo 'alias python=python3' >> ~/.bashrc
+#source ~/.bashrc
 
 # upgrade pip and install wheel
 pip install -U pip
@@ -31,3 +38,4 @@ pip install -r requirements.txt
 
 echo "Python3 has been aliased to python so you can now run python3 by running python"
 echo "You can undo this change by removing the alias from your ~/.bashrc file"
+
