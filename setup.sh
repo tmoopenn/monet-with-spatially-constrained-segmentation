16d67db2539f061458ed6f3fc9fcbcb858d7ad65
tmoopenn <tj-mp@hotmail.com>
2020-04-07 19:42:55 +0000

    update setup files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
-# PyTorch and scikit learn
-pip install scikit-learn
+# configure system and python dependencies 
+sudo apt update
+sudo apt-get -y install build-essential checkinstall software-properties-common
+sudo apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+# install python
+sudo apt-get -y install python3-dev
+py_version=$(python3 --version)
+echo "Installed python version $py_version"
+
+# alias python and add to bashrc file
+echo 'alias python=python3.7' >> ~/.bashrc
+
+# upgrade pip and install wheel
+pip install -U pip
+pip install wheel
 
 # Baselines for Atari preprocessing
 # Tensorflow is a dependency, but you don't need to install the GPU version
@@ -10,8 +24,10 @@ pip install git+git://github.com/openai/baselines
 pip install git+git://github.com/ankeshanand/pytorch-a2c-ppo-acktr-gail
 
 # Clone and install package
-pip install -r atari-representation-learning/requirements.txt
 pip install git+git://github.com/mila-iqia/atari-representation-learning.git
 
 # install requirements file 
 pip install -r requirements.txt
+
+echo "Python3 has been aliased to python so you can now run python3 by running python"
+echo "You can undo this change by removing the alias from your ~/.bashrc file"
