fab48e715bc2cc163cda99725277e9957e654153
Rashi Dhar <rdhar1997@gmail.com>
2020-04-12 21:16:17 -0400

    feat: add visualizer

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from train_options import TrainOptions
 from models import create_model
 from data import create_dataset
 import util
-#from util.visualizer import Visualizer
+from visualizer import Visualizer 
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
-    #visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
+    visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
     total_iters = 0                # the total number of training iterations
 
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
-            #visualizer.reset()
+            visualizer.reset()
             total_iters += opt.batch_size
             epoch_iter += opt.batch_size
 
@@ -47,14 +47,14 @@ if __name__ == '__main__':
             if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
-                #visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
-                #visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
-                #if opt.display_id > 0:
-                    #visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                if opt.display_id > 0:
+                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
 
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
                 print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
