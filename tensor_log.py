4fba927405b3d3b69f0e56e01024d3653e28b328
Soma A. Hota <shnasabrainwave@gmail.com>
2020-04-14 00:57:45 -0400

    Add files via upload

---
+++ tensor_log.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+
+# Writer will output to ./runs/ directory by default
+writer = SummaryWriter(/home/soma_arunkanti_hota/Proj/unsupervised-object-detection-with-rpn/checkpoints/experiment_name/loss_log.txt_)
+writer.close()
