817d2cd91556bd136cd94c8a7793c7c7fefbb367
Soma A. Hota <shnasabrainwave@gmail.com>
2020-04-14 01:46:55 -0400

    testing torch.utils.tensorboard

--- tensor_log.py
+++ tensor_log.py
@@ -3,5 +3,5 @@ import torchvision
 from torch.utils.tensorboard import SummaryWriter
 
 # Writer will output to ./runs/ directory by default
-writer = SummaryWriter(/home/soma_arunkanti_hota/Proj/unsupervised-object-detection-with-rpn/checkpoints/experiment_name/loss_log.txt_)
+writer = SummaryWriter(/unsupervised-object-detection-with-rpn/checkpoints/experiment_name/loss_log.txt_)
 writer.close()
