25e3fa99e939c0d0148d8d381cc7ca0f551628dd
Rashi Dhar <rdhar1997@gmail.com>
2020-04-26 11:40:20 -0400

    add transporter

--- models/transporter/run.sh
+++ models/transporter/run.sh
@@ -1,5 +1,5 @@
 
 python3 -m venv transporter-venv
 source transporter-venv/bin/activate
-pip install -r transporter/requirements.txt
+pip install -r requirements.txt
 python -m transporter.transporter_test
