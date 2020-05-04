8f0bb050238115f45f74b05019322da4a8c797e4
Soma A. Hota <shnasabrainwave@gmail.com>
2020-05-04 16:37:20 -0400

    Update atari_dataset.py

--- data/atari_dataset.py
+++ data/atari_dataset.py
@@ -3,8 +3,9 @@ import torchvision.transforms.functional as TF
 import numpy as np
 from torch.utils.data import RandomSampler, BatchSampler
 import torch
-import util 
+from utils import util
 
+from data.image_folder import make_dataset
 from data.base_dataset import BaseDataset
 from PIL import Image
 from .episodes import get_episodes
@@ -17,11 +18,12 @@ class AtariDataset(BaseDataset):
     @staticmethod
     def modify_commandline_options(parser, is_train):
         parser.set_defaults(input_nc=3, output_nc=3,
-                            crop_size=180, # crop is done first
-                            load_size=128,  # before resize
+                            crop_size=160, # crop is done first
+                            load_size=64,  # before resize
                             num_slots=7, display_ncols=7)
         parser.add_argument('--collect_mode', type=str, default='random_agent', help='Specifies whether agent in atari should be random or pretrained agent (pretrained_ppo)')
         parser.add_argument('--game', type=str, default='SpaceInvadersNoFrameskip-v4', help='Atari game to gather frames from')
+        parser.add_argument('--dynamic_datagen', action='store_true', help='flag indicating whether train batches will be generated dynamically')
         return parser
 
     def __init__(self, opt):
@@ -30,7 +32,20 @@ class AtariDataset(BaseDataset):
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseDataset.__init__(self, opt)
-        self.A_imgs = self.generate_epoch_episodes()
+        self.use_pednet = opt.use_pednet
+        self.dynamic_datagen = opt.dynamic_datagen
+        self.A_paths = None
+        if opt.dynamic_datagen:
+            if opt.use_pednet:
+                self.A_imgs = self.generate_epoch_episodes_multi_frame()
+            else:
+                self.A_imgs = self.generate_epoch_episodes()
+        else:
+            BaseDataset.__init__(self, opt)
+            p = os.path.join(opt.dataroot, 'images', 'train' if opt.isTrain else 'test')
+            self.A_paths = sorted(make_dataset(p, opt.max_dataset_size))
+            if opt.use_pednet:
+                self.A_paths = [self.A_paths[i:i+3] for i in range(len(self.A_paths)-3)]
 
     def _transform(self, img):
         '''
@@ -40,7 +55,7 @@ class AtariDataset(BaseDataset):
         '''
         # crop at top left corner (0,34) for crop box size of (self.opt.crop_size, self.opt.crop_size) then resize cropped image to self.opt.load_size 
         # expects image in PIL format 
-        img = TF.resized_crop(img,  20, 0, self.opt.crop_size, self.opt.crop_size, self.opt.load_size, Image.BILINEAR)
+        img = TF.resized_crop(img,  0, 0, self.opt.crop_size, self.opt.crop_size, self.opt.load_size, Image.NEAREST)
         img = TF.to_tensor(img)
 
         # should we normalize with mean of 0 and st.dev 1??
@@ -55,14 +70,38 @@ class AtariDataset(BaseDataset):
             A(tensor) - - an image in one domain
             A_paths(str) - - the path of the image
         """
-        A_img = self.A_imgs[index]
-        A_img = TF.to_pil_image(A_img)
-        A = self._transform(A_img)
-        return {'A': A, 'A_paths': ""}
+        path = ""
+        if self.use_pednet:
+            if not self.dynamic_datagen:
+                A_path = self.A_paths[index]
+                path = A_path
+                A_img_prev, A_img_t, A_img_next = Image.open(A_path[0]).convert('RGB'), Image.open(A_path[1]).convert('RGB'), \
+                                Image.open(A_path[2]).convert('RGB')
+            else:
+                A_img = self.A_imgs[index]
+                A_img_prev, A_img_t, A_img_next = A_img[:3,:,:], A_img[3:6,:,:], A_img[6:,:,:]
+                A_img_prev, A_img_t, A_img_next = TF.to_pil_image(A_img_prev), TF.to_pil_image(A_img_t), TF.to_pil_image(A_img_next)
+            A_prev, A_t, A_next = self._transform(A_img_prev), self._transform(A_img_t), self._transform(A_img_next)
+            A = torch.cat((A_prev, A_t, A_next),0)
+            return {'A':A, 'A_paths':path}
+        else:
+            if not self.dynamic_datagen:
+                A_path = self.A_paths[index]
+                path = A_path
+                A_img = Image.open(A_path).convert('RGB')
+            else:
+                A_img = self.A_imgs[index]
+                A_img = TF.to_pil_image(A_img)
+            A = self._transform(A_img)
+            path = "" if not self.A_paths else self.A_paths
+            return {'A': A, 'A_paths': path} 
+      
 
     def __len__(self):
         """Return the total number of images in the dataset."""
-        return len(self.A_imgs)
+        if self.dynamic_datagen:
+            return len(self.A_imgs)
+        return len(self.A_paths)
     
     def generate_epoch_episodes(self):
         '''
@@ -78,6 +117,24 @@ class AtariDataset(BaseDataset):
             for frame in episode:
                 all_frames.append(frame)
         return all_frames
+
+    def generate_epoch_episodes_multi_frame(self):
+        '''
+        Generates a list of sequential frames across different episodes 
+        returns: [tensor (c,h,w)] -> a list of 3D tensors where each tensor represents a frame
+        '''
+        # [[torch tensors (3, h, w)]]
+        episodes = get_episodes(self.opt.game, self.opt.epoch_steps, collect_mode=self.opt.collect_mode)
+        total_steps = sum([len(e) for e in episodes])
+        print('Total Steps: {}'.format(total_steps))
+        all_frames = []
+        for episode in episodes:
+            for i in range(0,len(episode)-3,3):
+                x_prev, xt, x_next = episode[i:i+3]
+                x = torch.cat((x_prev, xt, x_next),0)
+                all_frames.append(x)
+        return all_frames
+                
     
     def generate_epoch_episodes_in_batches(self):
         '''
